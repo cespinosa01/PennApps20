41c5ec55b5c0448cd8277373419a9719bf501c7a
Cindy <01cindyespinosa@gmail.com>
2019-09-07 09:38:23 -0400

    html/FLASK/list to html

--- app/webapp/app.py
+++ app/webapp/app.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
+import boto3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    text="Sam"
-    return render_template('index.html', text=text)
+    imageFile='example_04.jpg'
+    client=boto3.client('rekognition')
+
+    with open(imageFile, 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+
+    print('x ' + imageFile)
+    #for label in response['Labels']:
+    #    labels= (label['Name'] + ' : ' + str(label['Confidence']))
+    #    print(str(labels))
+    
+    return render_template('index2.html', labels=response['Labels'])
 
 if __name__ == '__main__':
-    app.run()
+     app.run()
