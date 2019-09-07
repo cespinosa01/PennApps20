c4ea4517c4424ffcf715bd8c63e006ca97e24a35
malini <malinikun2020@gmail.com>
2019-09-07 11:04:13 -0400

    search

--- app/webapp/google.py
+++ app/webapp/google.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python
-import boto3
+from googlesearch import search
 from flask import Flask, render_template
-
+import boto3
 app = Flask(__name__)
-
+# to search
 @app.route("/")
 def index():
     imageFile='apple.jpg'
@@ -15,6 +14,10 @@ def index():
     print('x ' + imageFile)
 
     return render_template('index2.html', labels=response['Labels'])
+    query = response['Labels']
+    print(str(query))
+    #for j in search(query, tld="co.in", num=10, stop=1, pause=2):
+        #print(j)
 
 if __name__ == '__main__':
      app.run()
