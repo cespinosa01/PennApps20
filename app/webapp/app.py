39b2dd5e6c01ff1d66549cd847cf470a8dffbe98
malini <malinikun2020@gmail.com>
2019-09-08 00:42:31 -0400

    ML_stuff

--- app/webapp/app.py
+++ app/webapp/app.py
@@ -3,10 +3,10 @@
 import boto3
 from flask import Flask, render_template, request
 from googlesearch import search
-from banana.classify_images_01 import banana_function
-from potatoes.classify_images_02 import potato_function
-from apple.classify_images_03 import apple_function
-from bread.classify_images_04 import bread_function
+#from banana.classify_images_01 import banana_function
+#from potatoes.classify_images_02 import potato_function
+#from apple.classify_images_03 import apple_function
+#from bread.classify_images_04 import bread_function
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -38,16 +38,16 @@ def index():
         d[label]=list(recipes)
         label = response['Labels'][0]['Name']
         print(label)
-        if(label == "Food"):
-            banana_function("knn","3scenes","3scenes/ripe_new/ripe_01.jpg")
-        elif(label == "Apple"):
-            potato_function("knn","3scenes","3scenes/ripe_new/ripe_01.jpeg")
-        elif(label == "Fruit"):
-            apple_function("knn","3scenes", "3scenes/ripe_new/ripe_01.jpeg")
-        elif(label == "Plant"):
-            bread_function("knn","3scenes", "3scenes/bread/bread_01.jpg")
-        else:
-            print("Food")
+        #if(label == "Banana"):
+            #banana_function("knn","3scenes")
+        #elif(label == "Potato"):
+            #potato_function("knn","3scenes")
+        #elif(label == "Apple"):
+            #apple_function("knn","3scenes")
+        #elif(label == "Bread"):
+            #bread_function("knn","3scenes")
+        #else:
+            #print("Food")
         return render_template('index9.html', labels=response['Labels'], d=d)
     else:
         return render_template('index7.html')
--- app/webapp/banana/classify_images_01.py
+++ app/webapp/banana/classify_images_01.py
@@ -20,70 +20,68 @@ from imutils import paths
 import numpy as np
 import argparse
 import os
-def banana_function(model, dataset, picture):
-	def extract_color_stats(image):
-		# split the input image into its respective RGB color channels
-		# and then create a feature vector with 6 values: the mean and
-		# standard deviation for each of the 3 channels, respectively
-		(R, G, B) = image.split()
-		features = [np.mean(R), np.mean(G), np.mean(B), np.std(R),
-			np.std(G), np.std(B)]
+def extract_color_stats(image):
+	# split the input image into its respective RGB color channels
+	# and then create a feature vector with 6 values: the mean and
+	# standard deviation for each of the 3 channels, respectively
+	(R, G, B) = image.split()
+	features = [np.mean(R), np.mean(G), np.mean(B), np.std(R),
+		np.std(G), np.std(B)]
 
-		# return our set of features
-		return features
+	# return our set of features
+	return features
 
-	# define the dictionary of models our script can use, where the key
-	# to the dictionary is the name of the model (supplied via command
-	# line argument) and the value is the model itself
-	models = {
-		"knn": KNeighborsClassifier(n_neighbors=1),
-		"naive_bayes": GaussianNB(),
-		"logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
-		"svm": SVC(kernel="linear"),
-		"decision_tree": DecisionTreeClassifier(),
-		"random_forest": RandomForestClassifier(n_estimators=5),
-		"mlp": MLPClassifier()
-	}
+# define the dictionary of models our script can use, where the key
+# to the dictionary is the name of the model (supplied via command
+# line argument) and the value is the model itself
+models = {
+	"knn": KNeighborsClassifier(n_neighbors=1),
+	"naive_bayes": GaussianNB(),
+	"logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
+	"svm": SVC(kernel="linear"),
+	"decision_tree": DecisionTreeClassifier(),
+	"random_forest": RandomForestClassifier(n_estimators=5),
+	"mlp": MLPClassifier()
+}
 
-	# grab all image paths in the input dataset directory, initialize our
-	# list of extracted features and corresponding labels
-	print("[INFO] extracting image features...")
-	imagePaths = paths.list_images("banana/"+dataset)
-	data = []
-	labels = []
+# grab all image paths in the input dataset directory, initialize our
+# list of extracted features and corresponding labels
+print("[INFO] extracting image features...")
+imagePaths = paths.list_images("banana/"+dataset)
+data = []
+labels = []
 
-	# loop over our input images
-	for imagePath in imagePaths:
-		# load the input image from disk, compute color channel
-		# statistics, and then update our data list
-		image = Image.open(imagePath)
-		features = extract_color_stats(image)
-		data.append(features)
+# loop over our input images
+for imagePath in imagePaths:
+	# load the input image from disk, compute color channel
+	# statistics, and then update our data list
+	image = Image.open(imagePath)
+	features = extract_color_stats(image)
+	data.append(features)
 
-		# extract the class label from the file path and update the
-		# labels list
-		label = imagePath.split(os.path.sep)[-2]
-		labels.append(label)
+	# extract the class label from the file path and update the
+	# labels list
+	label = imagePath.split(os.path.sep)[-2]
+	labels.append(label)
 
-	# encode the labels, converting them from strings to integers
-	le = LabelEncoder()
-	labels = le.fit_transform(labels)
+# encode the labels, converting them from strings to integers
+le = LabelEncoder()
+labels = le.fit_transform(labels)
 
-	# perform a training and testing split, using 75% of the data for
-	# training and 25% for evaluation
-	(trainX, testX, trainY, testY) = train_test_split(data, labels,
-		test_size=0.25)
+# perform a training and testing split, using 75% of the data for
+# training and 25% for evaluation
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.25)
 
-	# train the model
-	print("[INFO] using '{}' model".format(model))
-	model = models[model]
-	model.fit(trainX, trainY)
+# train the model
+print("[INFO] using '{}' model".format(model))
+model = models[model]
+model.fit(trainX, trainY)
 
-	# make predictions on our data and show a classification report
-	print("[INFO] evaluating...")
-	predictions = model.predict(testX)
-	print(classification_report(testY, predictions,
-		target_names=le.classes_))
-	result = model.predict_picture(picture,batch_size=10)
-	print(result)
-	return 1
+# make predictions on our data and show a classification report
+print("[INFO] evaluating...")
+predictions = model.predict(testX)
+print(classification_report(testY, predictions,
+	target_names=le.classes_))
+result = model.predict_picture(picture,batch_size=10)
+print(result)
